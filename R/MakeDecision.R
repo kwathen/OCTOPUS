4e313cb952d66c43e9b42011bdd807c42f9c335c
kwathen <kwathen@its.jnj.com>
2019-01-10 14:10:01 -0600

    Update to license file

--- R/MakeDecision.R
+++ R/MakeDecision.R
@@ -2,7 +2,8 @@
 #
 # Copyright (C) 2018 JANSSEN RESEARCH & DEVELOPMENT, LLC
 # This package is governed by the JRD Platform Trial Simulation License, which is the
-# GNU General Public License V3 with additional terms. The precise license terms are located in LICENSE and GPL.
+# GNU General Public License V3 with additional terms. The precise license terms are located in the files
+# LICENSE and GPL.
 #
 ##################################################################################################################
 
